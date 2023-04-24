dfb584a98ae85d26461bc9bd6202d92f2ff25e1c
lj140542 <jonathan.lefebvre76600@gmail.com>
2023-04-24 15:35:50 +0900

    Creates the default files

---
+++ main.js
@@ -0,0 +1,9 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+console.log('Hello from the console!');
+/******/ })()
+;
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoibWFpbi5qcyIsIm1hcHBpbmdzIjoiOzs7OztBQUFBLHVDIiwic291cmNlcyI6WyJ3ZWJwYWNrOi8vb2Rpbi1yZXN0YXVyYW50LXBhZ2UvLi9zcmMvaW5kZXguanMiXSwic291cmNlc0NvbnRlbnQiOlsiY29uc29sZS5sb2coJ0hlbGxvIGZyb20gdGhlIGNvbnNvbGUhJyk7Il0sIm5hbWVzIjpbXSwic291cmNlUm9vdCI6IiJ9
